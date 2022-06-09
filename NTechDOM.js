c7bad799a5dfd476a663f307a67c1b3f6e9f2651
saffiullahfahim <fahim14@student.sust.edu>
2022-06-10 03:41:42 +0600

    reset method add

--- NTechDOM.js
+++ NTechDOM.js
@@ -2,140 +2,163 @@ const NTechDOM = {
   version: "1.0",
   node: 0,
   rendered: false,
-  createElement : (element = "div", childrens = [], attributes = {}) => {
-    if(attributes.constructor.toString().indexOf("Object") >= 0){
-      for(let x in attributes){
-        if(attributes[x].constructor.toString().indexOf("Array") < 0){
+  createElement: (element = "div", childrens = [], attributes = {}) => {
+    if (attributes.constructor.toString().indexOf("Object") >= 0) {
+      for (let x in attributes) {
+        if (attributes[x].constructor.toString().indexOf("Array") < 0) {
           attributes[x] = String(attributes[x]).split(" ");
         }
       }
     } else attributes = {};
     const object = {
       _name: element,
-      _childrens: (childrens.constructor.toString().indexOf("Array") >= 0) ? childrens : [childrens],
+      _childrens:
+        childrens.constructor.toString().indexOf("Array") >= 0
+          ? childrens
+          : [childrens],
       _attributes: attributes,
       _reuse: false,
-      append(...childrens){
+      append(...childrens) {
         this._childrens = this._childrens.concat(childrens);
-        if(NTechDOM.rendered && !this._reuse){
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      setChildren(childrens){
-        this._childrens = (childrens.constructor.toString().indexOf("Array") >= 0) ? childrens : [childrens];
-        if(NTechDOM.rendered && !this._reuse){
+      setChildren(childrens) {
+        this._childrens =
+          childrens.constructor.toString().indexOf("Array") >= 0
+            ? childrens
+            : [childrens];
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      getChildren(children){
-        return (this._childrens[children]) ? this._childrens[children] : "";
+      getChildren(children) {
+        return this._childrens[children] ? this._childrens[children] : "";
       },
-      removeChildren(children){
+      removeChildren(children) {
         let arr = [...this._childrens];
-        this._childrens = arr.slice(0, children).concat(arr.slice(children + 1, arr.length));
-        if(NTechDOM.rendered && !this._reuse){
+        this._childrens = arr
+          .slice(0, children)
+          .concat(arr.slice(children + 1, arr.length));
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      insert(start, ...children){
+      insert(start, ...children) {
         let arr = [...this._childrens];
-        this._childrens = [...arr.slice(0, start), ...children, ...arr.slice(start, arr.length)];
-        if(NTechDOM.rendered && !this._reuse){
+        this._childrens = [
+          ...arr.slice(0, start),
+          ...children,
+          ...arr.slice(start, arr.length),
+        ];
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      removeElement(element){
+      removeElement(element) {
         let arr = [...this._childrens];
-        for(let children = 0; children <  arr.length; children++){
-          if(arr[children].constructor.toString().indexOf("Object") >= 0){
-            if(arr[children]._node == element._node){
-              this._childrens = arr.slice(0, children).concat(arr.slice(children + 1, arr.length));
-            } else{
+        for (let children = 0; children < arr.length; children++) {
+          if (arr[children].constructor.toString().indexOf("Object") >= 0) {
+            if (arr[children]._node == element._node) {
+              this._childrens = arr
+                .slice(0, children)
+                .concat(arr.slice(children + 1, arr.length));
+            } else {
               arr[children].removeElement(element);
             }
           }
         }
-        if(NTechDOM.rendered && !this._reuse){
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      addAttribute(key, ...values){
-        if(this._attributes[key]){
+      addAttribute(key, ...values) {
+        if (this._attributes[key]) {
           this._attributes[key] = [...this._attributes[key], ...values];
-        }
-        else{
+        } else {
           this._attributes[key] = values;
         }
-        if(NTechDOM.rendered && !this._reuse){
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      removeAttribute(...keys){
-        for(let x of keys){
+      removeAttribute(...keys) {
+        for (let x of keys) {
           delete this._attributes[x];
         }
-        if(NTechDOM.rendered && !this._reuse){
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      changeAttribute(key, ...values){
-        if(this._attributes[key]){
+      changeAttribute(key, ...values) {
+        if (this._attributes[key]) {
           this._attributes[key] = [...values];
-        }
-        else{
+        } else {
           this._attributes[key] = values;
         }
-        if(NTechDOM.rendered && !this._reuse){
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      changeAttributeN(key, ...values){
-        if(this._attributes[key]){
+      changeAttributeN(key, ...values) {
+        if (this._attributes[key]) {
           this._attributes[key] = [...values];
-        }
-        else{
+        } else {
           this._attributes[key] = values;
         }
         return this;
       },
-      setAttribute(attributes){
-        if(attributes.constructor.toString().indexOf("Object") >= 0){
-          for(let x in attributes){
-            if(attributes[x].constructor.toString().indexOf("Array") < 0){
+      setAttribute(attributes) {
+        if (attributes.constructor.toString().indexOf("Object") >= 0) {
+          for (let x in attributes) {
+            if (attributes[x].constructor.toString().indexOf("Array") < 0) {
               attributes[x] = String(attributes[x]).split(" ");
             }
           }
         } else attributes = {};
         this._attributes = attributes;
-        if(NTechDOM.rendered && !this._reuse){
+        if (NTechDOM.rendered && !this._reuse) {
           NTechDOM.rerender(this);
         }
         return this;
       },
-      getAttribute(attribute){
-        return (this._attributes[attribute]) ? this._attributes[attribute] : "";
+      getAttribute(attribute) {
+        return this._attributes[attribute] ? this._attributes[attribute] : "";
       },
-      _customFunction(){
-        
-      },
-      setCustomFunction(_fuction = () => {}){
-        if(_fuction.constructor.toString().indexOf("Function") >= 0) this._customFunction = _fuction;
+      _customFunction() {},
+      setCustomFunction(_fuction = () => {}) {
+        if (_fuction.constructor.toString().indexOf("Function") >= 0)
+          this._customFunction = _fuction;
         return this;
       },
-      _render(){
+      reset() {
+        let arr = [...this._childrens];
+        for (let children = 0; children < arr.length; children++) {
+          if (arr[children].constructor.toString().indexOf("Object") >= 0) {
+            if (arr[children]._name == "input") {
+              arr[children].changeAttribute("value", "");
+            } else {
+              arr[children].reset();
+            }
+          }
+        }
+        return this;
+      },
+      _render() {
         let childrens = "";
         this._node = NTechDOM.node++;
         let element = this._name;
         this._childrens.forEach((value) => {
-          if(value.constructor.toString().indexOf("Object") >= 0){
+          if (value.constructor.toString().indexOf("Object") >= 0) {
             childrens += value._render();
           } else {
             childrens += value;
@@ -145,55 +168,57 @@ const NTechDOM = {
         this._rendered = true;
         let _attributes = this._attributes;
         let attributes = "";
-        for(let x in _attributes){
-          attributes += (x + '="' + _attributes[x].join(" ") + '" ');
+        for (let x in _attributes) {
+          attributes += x + '="' + _attributes[x].join(" ") + '" ';
         }
         return `<${element} ${attributes}>${childrens}</${element}>`;
       },
-      _rerender(){
+      _rerender() {
         let childrens = "";
         this._childrens.forEach((value) => {
-          if(value.constructor.toString().indexOf("Object") >= 0){
+          if (value.constructor.toString().indexOf("Object") >= 0) {
             childrens += value._render();
           } else {
             childrens += value;
           }
         });
         return childrens;
-      }
+      },
     };
     return object;
   },
-  render(id = "", element = ""){
-    if(id){
+  render(id = "", element = "") {
+    if (id) {
       this.id = id;
       this.element = element;
-      document.getElementById(id).innerHTML = (element) ? element._render([]) : element;
+      document.getElementById(id).innerHTML = element
+        ? element._render([])
+        : element;
       this.rendered = true;
       element._customFunction();
     }
   },
-  rerender(element){
+  rerender(element) {
     const node = document.querySelector(`[node="${element._node}"]`);
-    while(node.attributes.length){
+    while (node.attributes.length) {
       node.removeAttribute(node.attributes[0].name);
     }
     const attributes = element._attributes;
-    for(let x in attributes){
+    for (let x in attributes) {
       node.setAttribute(x, attributes[x].join(" "));
     }
     node.innerHTML = element._rerender();
     element._customFunction();
     //this.render(this.id, this.element)
   },
-  
+
   // time, post, get, readFiles, getBlobData64
-  time(){
+  time() {
     return fetch("https://worldtimeapi.org/api/timezone/Asia/Dhaka").then(
       (_res) => _res.text()
     );
   },
-  post(url, data){
+  post(url, data) {
     const form = new FormData();
     for (let x in data) {
       form.append(x, data[x]);
@@ -207,10 +232,10 @@ const NTechDOM = {
       body: form,
     }).then((_res) => _res.text());
   },
-  get(url){
+  get(url) {
     return fetch(url).then((_res) => _res.text());
   },
-  readFiles(...files){
+  readFiles(...files) {
     return new Promise((resolve, reject) => {
       data = [];
       const _reader = (file) => {
@@ -226,20 +251,20 @@ const NTechDOM = {
       _reader(0);
     });
   },
-  getBlobData64(url){
+  getBlobData64(url) {
     return fetch(url)
-    .then((response) => response.blob())
-    .then(
-      (blob) =>
-        new Promise((callback) => {
-          let reader = new FileReader();
-          reader.onload = function () {
-            callback(this.result);
-          };
-          reader.readAsDataURL(blob);
-        })
-    );
+      .then((response) => response.blob())
+      .then(
+        (blob) =>
+          new Promise((callback) => {
+            let reader = new FileReader();
+            reader.onload = function () {
+              callback(this.result);
+            };
+            reader.readAsDataURL(blob);
+          })
+      );
   },
 };
 
-export default NTechDOM ;
+export default NTechDOM;
